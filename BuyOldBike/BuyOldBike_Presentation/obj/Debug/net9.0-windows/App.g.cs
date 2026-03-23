a224971db9bc108c97ca9b9088049746ef934917
dangp2660 <Dangp2660@gmail.com>
2026-03-23 11:36:54 +0700

    up

--- BuyOldBike/BuyOldBike_Presentation/obj/Debug/net9.0-windows/App.g.cs
+++ BuyOldBike/BuyOldBike_Presentation/obj/Debug/net9.0-windows/App.g.cs
@@ -46,7 +46,7 @@ namespace BuyOldBike_Presentation {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.13.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.14.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -58,7 +58,7 @@ namespace BuyOldBike_Presentation {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/BuyOldBike_Presentation;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/BuyOldBike_Presentation;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -72,7 +72,7 @@ namespace BuyOldBike_Presentation {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.13.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.14.0")]
         public static void Main() {
             BuyOldBike_Presentation.App app = new BuyOldBike_Presentation.App();
             app.InitializeComponent();
